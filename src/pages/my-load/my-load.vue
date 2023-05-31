7dbc1a782fa12e5d6df4a47a09c6a3e9d6437e8d
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-05-31 22:21:30 +0600

    Refactor style my page

--- src/pages/my-load/my-load.vue
+++ src/pages/my-load/my-load.vue
@@ -1,10 +1,23 @@
 <template>
-  <h1>Name {{ name }}</h1>
+  <div class="pl-15 mb-5 d-flex align-start container__title">
+    <BaseIcon
+      name="drag"
+      width="20"
+      class="mt-5 menu-burger"
+      @click="openMenuWindow"
+    />
+    <h1 class="pa-2">{{ name }}</h1>
+  </div>
 </template>
 
 <script>
+import BaseIcon from "@/components/icons/BaseIcon.vue";
+import { mapActions } from "vuex";
+
 export default {
   name: "MyLoad",
+  components: { BaseIcon },
+
   data() {
     return {
       name: "",
@@ -16,5 +29,20 @@ export default {
   created() {
     this.name = this.$route.params.key;
   },
+
+  methods: {
+    ...mapActions(["setMenuWindow"]),
+
+    openMenuWindow() {
+      this.setMenuWindow(true);
+    },
+  },
 };
 </script>
+
+<style scoped>
+.container__title {
+  height: max-content;
+  padding-left: 20px !important;
+}
+</style>
